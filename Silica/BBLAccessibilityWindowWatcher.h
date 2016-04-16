85c2bd60625581ad8b47c5378a7486527ddcac5f
Andy Park <andy@bigbearlabs.com>
2016-04-17 00:46:10 +0200

    BBLAccessibilityWindowWatcher is a workaround for RubyMotion framework dependency resolution failure.

---
+++ Silica/BBLAccessibilityWindowWatcher.h
@@ -0,0 +1,26 @@
+//
+//  BBLAccessibilityWindowWatcher.h
+//  NMTest001
+//
+//  Created by ilo on 15/04/2016.
+//
+//
+
+#import <Foundation/Foundation.h>
+//#import <Silica/Silica.h>
+#import "SIWindow.h"
+#import "SIApplication.h"
+
+
+@interface BBLAccessibilityWindowWatcher : NSObject
+
+-(void) watchWindows;
+
+
+-(void) onFocusedWindowChanged:(SIWindow*)window;
+
+-(void) onWindowCreated:(SIWindow*)window;
+
+-(void) onApplicationActivated:(SIAccessibilityElement*)element;
+
+@end
